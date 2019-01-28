f5185b96cfc7ee1775361275ee2d90a72416c366
wonyung lee <wonyung21@naver.com>
2019-01-29 08:38:39 +0900

    author, affliation network 구축 추가 / attribute에 degree 추가

--- 1.Authors_network.py
+++ 1.Authors_network.py
@@ -20,22 +20,25 @@ df_T = df.T
 
 G  = nx.Graph()
 
+# co-authorship network construction 
+
 for i in range(len(df_T.index)):
     
-    corr_author = df_T['Corr_author'].iloc[i].split(",")
+#    corr_author = df_T['Corr_author'].iloc[i].split(",")
     auth = df_T['Author'].iloc[i].split(",")
-    
-    
+    year = df_T['Year'].iloc[i]
     
     for j in itertools.combinations(auth,2):
-        G.add_edge(*j)
+        G.add_edge(*j, year=year)
         
-
 G_edge = nx.to_pandas_edgelist(G)
 G_edge.to_excel('author_edge_list.xlsx')
 
 G_node = pd.DataFrame(list(G.nodes()))
-G_node.to_excel('author_list.xlsx')
+#G_node.to_excel('author_node_list.xlsx')
+
+degree_auth = pd.DataFrame(list(nx.degree(G)), columns=['index','degree'])
+degree_auth.to_excel('author_degree.xlsx')
         
 #G_nodes_frame = pd.DataFrame(G.nodes())
 #G_nodes_frame.to_excel('authors_list.xlsx') 
@@ -45,10 +48,31 @@ G_node.to_excel('author_list.xlsx')
 #    
     
 
-## Affliation network 구축
+
+
+## Affliation network construction
 
 G_aff = nx.Graph()
 
+# co-authorship network construction 
+
+for i in range(len(df_T.index)):
+    
+#    corr_author = df_T['Corr_author'].iloc[i].split(",")
+    auth_aff = df_T['Author'].iloc[i].split(",")
+    year = df_T['Year'].iloc[i]
+    
+    for j in itertools.combinations(auth_aff,2):
+        G_aff.add_edge(*j, year=year)
+        
+G_edge_aff = nx.to_pandas_edgelist(G_aff)
+G_edge_aff.to_excel('affliation_edge_list.xlsx')
+
+G_node_aff = pd.DataFrame(list(G_aff.nodes()))
+#G_node_aff.to_excel('affliation_node_list.xlsx')
+
+degree_aff = pd.DataFrame(list(nx.degree(G_aff)), columns=['index','degree'])
+degree_aff.to_excel('affliation_degree.xlsx')
 
 
 
